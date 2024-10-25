172755ff3b40d8fe8c719bb1e20b5917b213f860
nikitagordeev10 <nikitagordeev10@yandex.ru>
2024-10-25 19:56:53 +0300

    оформил для github

---
+++ app/src/main/java/com/example/lab_1/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.lab_1;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onMyButtonClick(View view)
+    {
+        // выводим сообщение
+        Toast.makeText(this, "Привет мир!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void startNewActivity(View v) {
+        Intent intent = new Intent(this, hello_world.class);
+        startActivity(intent);
+    }
+
+}
